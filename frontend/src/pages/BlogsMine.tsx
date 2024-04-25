1dde1b3cc9988b4450ad00cf14a2855a7ba330fd
Neerajd3vv <neerajwdev@gmail.com>
2024-04-25 18:22:41 +0530

    createfirstBlogButton

--- frontend/src/pages/BlogsMine.tsx
+++ frontend/src/pages/BlogsMine.tsx
@@ -3,8 +3,14 @@ import AppBar from "../components/AppBar";
 import MyBlogs from "../components/MyBlogs";
 import Skeleton from "../components/Skeleton";
 import AppBarLogged from "../components/AppBarLogged";
+import { useNavigate } from "react-router-dom";
 // import DummyAppBar from "../components/DummyAppbar";
 function BlogsMine() {
+  const navigate = useNavigate();
+  const navigateToCreate = () => {
+    navigate("/create-blog");
+  };
+
   const { userTokenExists } = useTokenExists();
   const { loading, myPersonalblog } = usePersonalBlogs();
   if (loading) {
@@ -51,7 +57,10 @@ function BlogsMine() {
                 first blog.
               </p>
               <div className="flex justify-center mt-6">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none">
+                <button
+                  onClick={navigateToCreate}
+                  className="bg-blue-600 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded focus:outline-none"
+                >
                   Create Blog
                 </button>
               </div>
