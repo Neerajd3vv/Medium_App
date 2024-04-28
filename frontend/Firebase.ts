e4a3480a445712fe1a6091ce4dd4099bd4412b05
Neerajd3vv <neerajwdev@gmail.com>
2024-04-29 00:12:10 +0530

    added-something-to-.env

--- frontend/Firebase.ts
+++ frontend/Firebase.ts
@@ -1,22 +1,23 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getStorage } from "firebase/storage";
+import {getStorage} from "firebase/storage"
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY, 
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // storage variable allow us to ref which storage we are talking about
-export const storage = getStorage(app);
+export const storage = getStorage(app)
