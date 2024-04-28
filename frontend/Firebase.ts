137cdf54ef70613b6ebd4e339c6d2d48d43a809c
Neerajd3vv <neerajwdev@gmail.com>
2024-04-28 23:01:48 +0530

    env for firebase

--- frontend/Firebase.ts
+++ frontend/Firebase.ts
@@ -1,23 +1,22 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-import {getStorage} from "firebase/storage"
+import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB5CTn-16Ej7X1ARvardQNDfb6jTkNGwE0",
-  authDomain: "imgaesmedium.firebaseapp.com",
-  projectId: "imgaesmedium",
-  storageBucket: "imgaesmedium.appspot.com",
-  messagingSenderId: "415956736227",
-  appId: "1:415956736227:web:3df57be49bd4c934b20dd6"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // storage variable allow us to ref which storage we are talking about
-export const storage = getStorage(app)
+export const storage = getStorage(app);
